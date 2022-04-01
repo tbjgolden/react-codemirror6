53db53fa1ce3cf28b57e28963b016f44ebeda6bc
andrebnassis <andrebnassis@gmail.com>
2022-03-31 21:55:38 -0300

    fix: add extension works

--- src/CodeMirrorLite/index.stories.tsx
+++ src/CodeMirrorLite/index.stories.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { Story, Meta } from '@storybook/react'
 import { CodeMirrorLite } from '.'
+import { action } from '@storybook/addon-actions'
+import { oneDark } from '@codemirror/theme-one-dark'
 
 export default {
   component: CodeMirrorLite,
@@ -112,6 +114,7 @@ const ControlledWrapper: Story<Parameters<typeof CodeMirrorLite>[0]> = (
             flex: '1 0 auto'
           }}
           value={value}
+          onChange={action('onChange')}
         />
       </div>
       input ↑ (invisible without extensions)
@@ -145,3 +148,57 @@ const ControlledWrapper: Story<Parameters<typeof CodeMirrorLite>[0]> = (
 }
 
 export const Controlled = ControlledWrapper.bind({})
+
+const AddExtensionSampleWrapper: Story<Parameters<typeof CodeMirrorLite>[0]> = (
+  args
+) => {
+  const [value, setValue] = useState(
+    'This text editor should use oneDark as its theme'
+  )
+
+  return (
+    <>
+      <div style={{ height: 200, display: 'flex' }}>
+        <CodeMirrorLite
+          {...args}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            flex: '1 0 auto'
+          }}
+          value={value}
+          onChange={setValue}
+          extensions={[oneDark]}
+        />
+      </div>
+      input ↑ (invisible without extensions)
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          setValue(
+            new Array(5)
+              .fill(0)
+              .map(() => Math.random().toString().slice(2))
+              .join('\n')
+          )
+        }}
+      >
+        change to random value
+      </button>
+      <br />
+      <br />
+      value:
+      <pre>
+        <code>{value}</code>
+      </pre>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: 'pre{margin-top:0;background:#eee;padding:16px}'
+        }}
+      />
+    </>
+  )
+}
+
+export const AddExtensionSample = AddExtensionSampleWrapper.bind({})
